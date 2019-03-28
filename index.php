f3dd5d41a7d8db7e534375d3dbb3f9e497103815
mru24 <mru24@yahoo.com>
2019-03-28 19:33:37 +0000

    Temperature converter edit

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
-
 require 'inc/conversions.php';
-
-if(isset($_POST['convert'])) {
-  if(!empty($_POST['unit']) && !empty($_POST['value'])) {
-    $value = preg_replace('/[^0-9]/', '', $_POST['value']);
-    $resultUnit = $_POST['unit'];
-    $result = $conv->temperature($resultUnit, $value);
-  } else {
-    $error = "Input some data";
-  }
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -20,60 +8,14 @@ if(isset($_POST['convert'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>PHP Unit Converter | WWProject Studio</title>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
   <link rel="stylesheet" href="css/style.min.css">
 </head>
 <body>
+  
   <div class="container">
-    <div class="temperature">
-      <h2>Temperature conversion</h2>
-      <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-        <input type="number" name="value" value="<?php echo $value; ?>">
-
-
-        <div class="radioButtons">
-          <label>
-            <input type="radio" name="unit" value="fahrenheit"
-              <?php if(isset($resultUnit) && $resultUnit == "fahrenheit") {
-                echo 'checked="checked"';
-              } ?>>
-            <div class="unitButton">Celsius</div>
-          </label>
-
-          <label>
-            <input type="radio" name="unit" value="celsius"
-            <?php if(isset($resultUnit) && $resultUnit == "celsius") {
-              echo 'checked="checked"';
-            } ?>>
-            <div class="unitButton">Fahrenheit</div>
-          </label>
-
-        </div>
-
-        <button type="submit" name="convert"><i class="fas fa-arrow-circle-right"></i> Convert</button>
-
-      </form>
-      <div class="display">
-
-        <?php
-
-        if(!empty($result)) {
-          echo $result;
-        } else {
-          if(isset($error)) {
-            echo $error;
-          }
-        }
-
-        ?>
-
-        <?php if(isset($resultUnit)) { ?>
-          <span>&deg; <?php echo $resultUnit; ?></span>
-        <?php } ?>
-
-      </div>
-    </div>
+    <?php include 'inc/temperature.php'; ?>
   </div>
 
 </body>
