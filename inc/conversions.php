9855429ed8fd66a93be580518d592d1694b9b104
mru24 <mru24@yahoo.com>
2019-04-01 14:53:52 +0100

    Converters edited

--- inc/conversions.php
+++ inc/conversions.php
@@ -68,310 +68,246 @@ class Conversions {
 
   // LENGTH
   public function length($resultUnit1, $resultUnit2, $value) {
-    $unit = array(
-      'millimetres',
-      'centimetres',
-      'metres',
-      'kilometres',
-      'inches',
-      'feet',
-      'yards',
-      'miles'
-    );
 
-    switch (true) {
-      // MILIMETRES TO CENTIMETRES
-      case($resultUnit1 == $unit[0] && $resultUnit2 == $unit[1]):
-        $result = $value * 0.1;
-        return $result;
-        break;
-      // MILIMETRES TO METRES
-      case($resultUnit1 == $unit[0] && $resultUnit2 == $unit[2]):
-        $result = $value * 0.001;
-        return $result;
-        break;
-      // MILIMETRES TO KILOMETRES
-      case($resultUnit1 == $unit[0] && $resultUnit2 == $unit[3]):
-        $result = $result = $value * 0.000001;
-        return $result;
-        break;
-      // MILIMETRES TO INCHES
-      case($resultUnit1 == $unit[0] && $resultUnit2 == $unit[4]):
-        $result = $value * 0.03937007874;
-        return $result;
-        break;
-      // MILIMETRES TO FEET
-      case($resultUnit1 == $unit[0] && $resultUnit2 == $unit[5]):
-        $result = $value * 0.0032808399;
-        return $result;
-        break;
-      // MILIMETRES TO YARDS
-      case($resultUnit1 == $unit[0] && $resultUnit2 == $unit[6]):
-        $result = $value * 0.0010936133;
-        return $result;
-        break;
-      // MILIMETRES TO MILES
-      case($resultUnit1 == $unit[0] && $resultUnit2 == $unit[7]):
-        $result = $value * 0.00000062137;
-        return $result;
-        break;
+    switch ($resultUnit1) {
+      case 'millimetres':
+        switch ($resultUnit2) {
+          case 'centimetres':
+            $result = $value / 10;
+            break;
+          case 'metres':
+            $result = $value / 1000;
+            break;
+          case 'kilometres':
+            $result = $value / 1000000;
+            break;
+          case 'inches':
+            $result = $value / 25.4;
+            break;
+          case 'feet':
+            $result = $value / 304.8;
+            break;
+          case 'yards':
+            $result = $value / 914.4;
+            break;
+          case 'miles':
+            $result = $value / 16093444;
+            break;
 
-        // CENTIMETRES TO MILIMETRES
-        case($resultUnit1 == $unit[1] && $resultUnit2 == $unit[0]):
-          $result = $value * 10;
-          return $result;
-          break;
-        // CENTIMETRES TO METRES
-        case($resultUnit1 == $unit[1] && $resultUnit2 == $unit[2]):
-          $result = $value * 0.01;
-          return $result;
-          break;
-        // CENTIMETRES TO KILOMETRES
-        case($resultUnit1 == $unit[1] && $resultUnit2 == $unit[3]):
-          $result = $value * 0.00001;
-          return $result;
-          break;
-        // CENTIMETRES TO INCHES
-        case($resultUnit1 == $unit[1] && $resultUnit2 == $unit[4]):
-          $result = $value * 0.3937007874;
-          return $result;
-          break;
-        // CENTIMETRES TO FEET
-        case($resultUnit1 == $unit[1] && $resultUnit2 == $unit[5]):
-          $result = $value * 0.032808399;
-          return $result;
-          break;
-        // CENTIMETRES TO YARDS
-        case($resultUnit1 == $unit[1] && $resultUnit2 == $unit[6]):
-          $result = $value * 0.010936133;
-          return $result;
-          break;
-        // CENTIMETRES TO MILES
-        case($resultUnit1 == $unit[1] && $resultUnit2 == $unit[7]):
-          $result = 0.00000621371192;
-          return $result;
-          break;
+          default:
+            // code...
+            break;
+        }
+        break;
+      case 'centimetres':
+        switch ($resultUnit2) {
+          case 'millimetres':
+            $result = $value * 10;
+            break;
+          case 'metres':
+            $result = $value / 100;
+            break;
+          case 'kilometres':
+            $result = $value / 100000;
+            break;
+          case 'inches':
+            $result = $value / 2.54;
+            break;
+          case 'feet':
+            $result = $value / 30.48;
+            break;
+          case 'yards':
+            $result = $value / 91.44;
+            break;
+          case 'miles':
+            $result = $value / 160934.4;
+            break;
 
-      // METRES TO MILIMETRES
-      case($resultUnit1 == $unit[2] && $resultUnit2 == $unit[0]):
-        $result = $value * 1000;
-        return $result;
+          default:
+            // code...
+            break;
+        }
         break;
-      // METRES TO CENTIMETRES
-      case($resultUnit1 == $unit[2] && $resultUnit2 == $unit[1]):
-        $result = $value * 100;
-        return $result;
-        break;
-      // METRES TO KILOMETRES
-      case($resultUnit1 == $unit[2] && $resultUnit2 == $unit[3]):
-        $result = $value * 0.001;
-        return $result;
-        break;
-      // METRES TO INCHES
-      case($resultUnit1 == $unit[2] && $resultUnit2 == $unit[4]):
-        $result = $value * 39.37007874;
-        return $result;
-        break;
-      // METRES TO FEET
-      case($resultUnit1 == $unit[2] && $resultUnit2 == $unit[5]):
-        $result = $value * 3.2808399;
-        return $result;
-        break;
-      // METRES TO YARDS
-      case($resultUnit1 == $unit[2] && $resultUnit2 == $unit[6]):
-        $result = $value * 1.0936133;
-        return $result;
-        break;
-      // METRES TO MILES
-      case($resultUnit1 == $unit[2] && $resultUnit2 == $unit[7]):
-        $result = 0.000621371192;
-        return $result;
-        break;
+      case 'metres':
+        switch ($resultUnit2) {
+          case 'millimetres':
+            $result = $value * 1000;
+            break;
+          case 'centimetres':
+            $result = $value * 100;
+            break;
+          case 'kilometres':
+            $result = $value / 1000;
+            break;
+          case 'inches':
+            $result = $value * 39.37;
+            break;
+          case 'feet':
+            $result = $value * 3.281;
+            break;
+          case 'yards':
+            $result = $value * 1.094;
+            break;
+          case 'miles':
+            $result = $value / 1609.344;
+            break;
 
-        // KILOMETRES TO MILIMETRES
-        case($resultUnit1 == $unit[3] && $resultUnit2 == $unit[0]):
-          $result = $value * 1000000;
-          return $result;
-          break;
-        // KILOMETRES TO CENTIMETRES
-        case($resultUnit1 == $unit[3] && $resultUnit2 == $unit[1]):
-          $result = $value * 100000;
-          return $result;
-          break;
-        // KILOMETRES TO METRES
-        case($resultUnit1 == $unit[3] && $resultUnit2 == $unit[2]):
-          $result = $value * 1000;
-          return $result;
-          break;
-        // KILOMETRES TO INCHES
-        case($resultUnit1 == $unit[3] && $resultUnit2 == $unit[4]):
-          $result = $value * 39370.07874;
-          return $result;
-          break;
-        // KILOMETRES TO FEET
-        case($resultUnit1 == $unit[3] && $resultUnit2 == $unit[5]):
-          $result = $value * 3280.8399;
-          return $result;
-          break;
-        // KILOMETRES TO YARDS
-        case($resultUnit1 == $unit[3] && $resultUnit2 == $unit[6]):
-          $result = $value * 1093.6133;
-          return $result;
-          break;
-        // KILOMETRES TO MILES
-        case($resultUnit1 == $unit[3] && $resultUnit2 == $unit[7]):
-          $result = 0.621371192;
-          return $result;
-          break;
+          default:
+            // code...
+            break;
+        }
+        break;
+      case 'kilometres':
+        switch ($resultUnit2) {
+          case 'millimetres':
+            $result = $value * 1000000;
+            break;
+          case 'centimetres':
+            $result = $value * 100000;
+            break;
+          case 'metres':
+            $result = $value * 1000;
+            break;
+          case 'inches':
+            $result = $value * 39370.079;
+            break;
+          case 'feet':
+            $result = $value * 3280.84;
+            break;
+          case 'yards':
+            $result = $value * 1093.613;
+            break;
+          case 'miles':
+            $result = $value / 1.609344;
+            break;
 
-          // INCHES TO MILIMETRES
-          case($resultUnit1 == $unit[4] && $resultUnit2 == $unit[0]):
+          default:
+            // code...
+            break;
+        }
+        break;
+      case 'inches':
+        switch ($resultUnit2) {
+          case 'millimetres':
             $result = $value * 25.4;
-            return $result;
             break;
-          // INCHES TO CENTIMETRES
-          case($resultUnit1 == $unit[4] && $resultUnit2 == $unit[1]):
+          case 'centimetres':
             $result = $value * 2.54;
-            return $result;
             break;
-          // INCHES TO METRES
-          case($resultUnit1 == $unit[4] && $resultUnit2 == $unit[2]):
-            $result = $value * 0.254;
-            return $result;
+          case 'metres':
+            $result = $value / 39.37;
             break;
-          // INCHES TO KILOMETRES
-          case($resultUnit1 == $unit[4] && $resultUnit2 == $unit[3]):
-            $result = $value * 0.0000254;
-            return $result;
+          case 'kilometres':
+            $result = $value / 39370.079;
             break;
-          // INCHES TO FEET
-          case($resultUnit1 == $unit[4] && $resultUnit2 == $unit[5]):
-            $result = $value * 0.083333333;
-            return $result;
+          case 'feet':
+            $result = $value / 12;
             break;
-          // INCHES TO YARDS
-          case($resultUnit1 == $unit[4] && $resultUnit2 == $unit[6]):
-            $result = $value * 0.0277777778;
-            return $result;
+          case 'yards':
+            $result = $value / 36;
             break;
-          // INCHES TO MILES
-          case($resultUnit1 == $unit[4] && $resultUnit2 == $unit[7]):
-            $result = 0.0000157828;
-            return $result;
+          case 'miles':
+            $result = $value / 63360;
             break;
 
-        // FEET TO MILIMETRES
-        case($resultUnit1 == $unit[5] && $resultUnit2 == $unit[0]):
-          $result = $value * 304.8;
-          return $result;
-          break;
-        // FEET TO CENTIMETRES
-        case($resultUnit1 == $unit[5] && $resultUnit2 == $unit[1]):
-          $result = $value * 30.48;
-          return $result;
-          break;
-        // FEET TO METRES
-        case($resultUnit1 == $unit[5] && $resultUnit2 == $unit[2]):
-          $result = $value * 0.3048;
-          return $result;
-          break;
-        // FEET TO KILOMETRES
-        case($resultUnit1 == $unit[5] && $resultUnit2 == $unit[3]):
-          $result = $value * 0.0003048;
-          return $result;
-          break;
-        // FEET TO INCHES
-        case($resultUnit1 == $unit[5] && $resultUnit2 == $unit[4]):
-          $result = $value * 12;
-          return $result;
-          break;
-        // FEET TO YARDS
-        case($resultUnit1 == $unit[5] && $resultUnit2 == $unit[6]):
-          $result = $value * 0.3333333333;
-          return $result;
-          break;
-        // FEET TO MILES
-        case($resultUnit1 == $unit[5] && $resultUnit2 == $unit[7]):
-          $result = 0.0001893932;
-          return $result;
-          break;
+          default:
+            // code...
+            break;
+        }
+        break;
+      case 'feet':
+        switch ($resultUnit2) {
+          case 'millimetres':
+            $result = $value * 304.8;
+            break;
+          case 'centimetres':
+            $result = $value * 30.48;
+            break;
+          case 'metres':
+            $result = $value / 3.281;
+            break;
+          case 'kilometres':
+            $result = $value / 3280.84;
+            break;
+          case 'inches':
+            $result = $value * 12;
+            break;
+          case 'yards':
+            $result = $value / 3;
+            break;
+          case 'miles':
+            $result = $value / 5280;
+            break;
 
-          // YARDS TO MILIMETRES
-          case($resultUnit1 == $unit[6] && $resultUnit2 == $unit[0]):
+          default:
+            // code...
+            break;
+        }
+        break;
+      case 'yards':
+        switch ($resultUnit2) {
+          case 'millimetres':
             $result = $value * 914.4;
-            return $result;
             break;
-          // YARDS TO CENTIMETRES
-          case($resultUnit1 == $unit[6] && $resultUnit2 == $unit[1]):
+          case 'centimetres':
             $result = $value * 91.44;
-            return $result;
             break;
-          // YARDS TO METRES
-          case($resultUnit1 == $unit[6] && $resultUnit2 == $unit[2]):
-            $result = $value * 0.9144;
-            return $result;
+          case 'metres':
+            $result = $value / 1.094;
             break;
-          // YARDS TO KILOMETRES
-          case($resultUnit1 == $unit[6] && $resultUnit2 == $unit[3]):
-            $result = $value * 0.0009144;
-            return $result;
+          case 'kilometres':
+            $result = $value / 1093.613;
             break;
-          // YARDS TO INCHES
-          case($resultUnit1 == $unit[6] && $resultUnit2 == $unit[4]):
+          case 'inches':
             $result = $value * 36;
-            return $result;
             break;
-          // YARDS TO FEET
-          case($resultUnit1 == $unit[6] && $resultUnit2 == $unit[5]):
+          case 'feet':
             $result = $value * 3;
-            return $result;
             break;
-          // YARDS TO MILES
-          case($resultUnit1 == $unit[6] && $resultUnit2 == $unit[7]):
-            $result = 0.0005681797;
-            return $result;
+          case 'miles':
+            $result = $value / 1760;
             break;
 
-        // MILES TO MILIMETRES
-        case($resultUnit1 == $unit[7] && $resultUnit2 == $unit[0]):
-          $result = $value * 1609350;
-          return $result;
-          break;
-        // MILES TO CENTIMETRES
-        case($resultUnit1 == $unit[7] && $resultUnit2 == $unit[1]):
-          $result = $value * 160935;
-          return $result;
-          break;
-        // MILES TO METRES
-        case($resultUnit1 == $unit[7] && $resultUnit2 == $unit[2]):
-          $result = $value * 1609.35;
-          return $result;
-          break;
-        // MILES TO KILOMETRES
-        case($resultUnit1 == $unit[7] && $resultUnit2 == $unit[3]):
-          $result = $value * 1.60935;
-          return $result;
-          break;
-        // MILES TO INCHES
-        case($resultUnit1 == $unit[7] && $resultUnit2 == $unit[4]):
-          $result = $value * 63360.23622;
-          return $result;
-          break;
-        // MILES TO FEET
-        case($resultUnit1 == $unit[7] && $resultUnit2 == $unit[5]):
-          $result = $value * 5280.019685;
-          return $result;
-          break;
-        // MILES TO YARDS
-        case($resultUnit1 == $unit[7] && $resultUnit2 == $unit[6]):
-          $result = 1760.0065617;
-          return $result;
-          break;
+          default:
+            // code...
+            break;
+        }
+        break;
+      case 'miles':
+        switch ($resultUnit2) {
+          case 'millimetres':
+            $result = $value * 1609344;
+            break;
+          case 'centimetres':
+            $result = $value * 160934.4;
+            break;
+          case 'metres':
+            $result = $value * 1609.344;
+            break;
+          case 'kilometres':
+            $result = $value * 1.609;
+            break;
+          case 'inches':
+            $result = $value * 63360;
+            break;
+          case 'feet':
+            $result = $value * 5280;
+            break;
+          case 'yards':
+            $result = $value * 1760;
+            break;
+
+          default:
+            // code...
+            break;
+        }
+        break;
 
       default:
         // code...
         break;
     }
+    return $result;
   }
 
   // WEIGHT
@@ -397,11 +333,11 @@ class Conversions {
             break;
 
           case('ounce'):
-            $result = $value * 0.000035274;
+            $result = $value / 28349.523;
             break;
 
           case('pound'):
-            $result = $value * 0.0000022046;
+            $result = $value / 453592.37;
             break;
 
           case('stoneUK'):
